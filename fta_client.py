5dcd0604d7cbad6154fbbb5720d66605ddcc2a0d
Keegan Nesbitt <knesbitt7@gatech.edu>
2014-11-13 22:11:59 -0500

    Added the start of the FTA Client and Server files. Nothing in the files has been tested.

---
+++ fta_client.py
@@ -0,0 +1,71 @@
+# The client application for the File Transfer Application (FTA) portion of the assignment
+#
+# Author: Keegan Nesbitt
+# Last Modified: 11/13/2014
+# CourseL CS 3251 B
+
+# Import the file containing the functions for the RTP protocol and any other necessary modules
+import RTP
+import sys
+
+# Retrieve the values from the command line arguments
+clientPortNumber = sys.argv[1]
+netEmuIP = sys.argv[2]
+netEmuPort = sys.argv[3]
+
+# Declare a variable that can later be used for an RTP socket
+socket = None
+windowSize = 100
+currentSegmentsOut = 0 # This field will be used to ensure the window size is not exceeded
+
+while True:
+    # Check for the user to input commands
+    userInput = raw_input('Enter a command for the FTA client:\n')
+
+    # Check for the type of command input by the user
+    command = user_input.split(' ')[0]
+    if command == 'connect':
+        connect()
+    elif command == 'get':
+        retrieveFile(user_input.split(' ')[1])
+    elif command == 'put':
+        sendFile(user_input.split(' ')[1])
+    elif command == 'window':
+        setWindowSize(int(user_input.split(' ')[1]))
+    elif command == 'disconnect':
+        disconnect()
+    elif command == 'exit':
+        sys.exit()
+    else:
+        print 'Not a valid command'
+
+# Connects to the FTA server running on the same IP host
+def connect():
+    # Create an RTP socket to be used
+    socket = RTP.createRTPSocket()
+    
+    # Form a connection with the server
+    socket.connectRTP(("127.0.0.1", clientPortNumber + 1))
+
+    # Print a confirmation to the user
+    print 'Connection formed successfully'
+
+# This function retrieves the specified file from the server
+def retrieveFile(filename):
+    
+
+# This function sends a file to the server
+def sendFile(filename):
+
+
+# This function will be used to modify the window size being used by the application
+def setWindowSize(newWindowSize):
+    windowSize = newWindowSize
+
+# This function is used to close the connection to the server
+def disconnect()
+    # Disconnect from the server
+    socket.closeRTPSocket()
+
+    # Print a confirmation to the user
+    print 'Successfully disconnected from server'
---
+++ fta_server.py
@@ -0,0 +1,46 @@
+# The server application for the File Transfer Application (FTA) portion of the assignment
+#
+# Author: Keegan Nesbitt
+# Last Modified: 11/13/2014
+# Course: CS 3251 B
+
+# Import the file containing the functions for the RTP protocol and any other necessary modules
+import RTP
+import sys
+
+# Create a variable to contain a socket and other necessary variables
+socket = None
+windowSize = 100
+currentSegmentsOut = 0 # This field will be used to ensure the window size is not exceeded
+
+# Get the command line arguments when the fta-server.py script is run, and initialize the server
+initializeServer(sys.argv[3], sys.argv[2], sys.argv[1])
+
+# Include some way to both check for the user input commands and checking for incoming connections
+
+
+# This function will be used to initialize the server with the specified options
+def initiatlizeServer(netEmuPort, netEmuIP, portNumber):
+    # Set the necessary configuration values from the defaults and parameter
+	netEmuPortNumber = netEmuPort
+	netEmuIPAddress = netEmuIP
+    serverPortNumber = portNumber
+
+    # Create a socket for the server
+    global socket
+    socket = RTP.createRTPSocket()
+
+    # Bind the socket to a the local address
+    socket.bindRTPSocket(("127.0.0.1", serverPortNumber))
+
+    # Allow the socket to accept incoming connections
+    socket.listenRTP()
+
+# This function will be used to modify the window size being used by the application
+def setWindowSize(newWindowSize):
+    windowSize = newWindowSize
+
+# This function will be used to gracefully shutdown the FTA-Server
+def terminateServer():
+    global socket
+    socket.closeRTPSocket()
