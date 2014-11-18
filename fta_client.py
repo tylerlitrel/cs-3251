cce5bbb2e77b31167ecbd093210c4333cf5b8e9b
Keegan Nesbitt <knesbitt7@gatech.edu>
2014-11-17 21:35:24 -0500

    Added some additional code to fta_client.py

--- fta_client.py
+++ fta_client.py
@@ -1,7 +1,7 @@
 # The client application for the File Transfer Application (FTA) portion of the assignment
 #
 # Author: Keegan Nesbitt
-# Last Modified: 11/13/2014
+# Last Modified: 11/17/2014
 # CourseL CS 3251 B
 
 # Import the file containing the functions for the RTP protocol and any other necessary modules
@@ -52,15 +52,40 @@ def connect():
 
 # This function retrieves the specified file from the server
 def retrieveFile(filename):
-    
+    # First tell the server that it needs to send a file
+    sendRTP('serverToClient')
+
+    # Receive the file from the server
+    fileByteArray = receiveRTP(8000000)
+
+    # Write the file to the directory
+    file = open(filename, 'wb')
+    file.write(fileByteArray)
+
+    # Print a confirmation to the user
+    print 'File has been received from the server'
 
 # This function sends a file to the server
 def sendFile(filename):
+    # First tell the server that it is receiving a file
+    sendRTP('clientToServer')
 
+    # Load the file into a byte array
+    fileByteArray = open(filename, 'rb').read()
+
+    # Send the file to the server
+    sendRTP(fileByteArray)
+
+    # Print a confirmation to the user
+    print 'File has been sent to the server'
 
 # This function will be used to modify the window size being used by the application
 def setWindowSize(newWindowSize):
+    # Update the window size
     windowSize = newWindowSize
+
+    # Print a confirmation to the user
+    print 'The window size has been updated'
 
 # This function is used to close the connection to the server
 def disconnect()
