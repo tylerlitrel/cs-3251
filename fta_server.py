f731d502549a0541ebfa81c03736a7b1398b73d4
Tyler Litrel <plitrel3@gatech.edu>
2014-11-23 13:49:56 -0500

    server changes

--- fta_server.py
+++ fta_server.py
@@ -2,7 +2,7 @@
 #
 # Author: Keegan Nesbitt
 # Last Modified: 11/13/2014
-# Course: CS 3251 B
+# Course: CS 3251 
 
 # Import the file containing the functions for the RTP protocol and any other necessary modules
 import RTP
@@ -22,16 +22,12 @@ while True:
     # Check for the user to input commands
     userInput = raw_input('Enter a command for the FTA client:\n')
 
-    #port number
-    #net emu ip address
-    #net emu port number
-    
     # Check for the type of command input by the user
     command = user_input.split(' ')[0]
     if command == 'terminate':
-        socket.close()
+        terminateServer()
     elif command == 'window':
-        socket.setMaxWindowSize(user_input.split(' ')[1])
+        setWindowSize(user_input.split(' ')[1])
     elif command == 'exit':
         sys.exit()
     else:
@@ -53,11 +49,14 @@ def initiatlizeServer(netEmuPort, netEmuIP, portNumber):
 
     # Allow the socket to accept incoming connections
     socket.listenRTP()
+    socket.accept()
 
 # This function will be used to modify the window size being used by the application
 def setWindowSize(newWindowSize):
     windowSize = newWindowSize
+    global socket
     socket.setMaxWindowSize(newWindowSize)
+
 # This function will be used to gracefully shutdown the FTA-Server
 def terminateServer():
     global socket
